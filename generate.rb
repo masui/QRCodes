0947badb9437c5c538e5020167d1010042091e40
Toshiyuki Masui <masui@pitecan.com>
2024-01-14 09:53:02 +0900

    ...

--- generate.rb
+++ generate.rb
@@ -1,5 +1,9 @@
 #
 # QRコードを並べたページを生成
+# A-Oneの65枚シート用
+# https://www.a-one.co.jp/product/search/detail.php?id=78265
+#
+# makeしてできるqrs.htmlを印刷する
 #
 
 puts <<EOF
@@ -11,14 +15,14 @@ puts <<EOF
 EOF
 
 imagesize = 50
-ysize = 74
-xsize = 140
+ysize = 72
+xsize = 138
 
 (0...13).each { |y|
   (0...5).each { |x|
     i = y * 5 + x + 1
-    puts "  <img src='images/#{i}.png' style='width:#{imagesize}px; height:#{imagesize}px; position:absolute; top:#{y*ysize+30}px; left:#{30+x*xsize}px;'>"
-    puts "  <span style='position:absolute; top:#{y*ysize+50}px; left:#{x*xsize+ysize+6}px; font-size:10px;'>#{i}</span>"
+    puts "    <img src='images/#{i}.png' style='width:#{imagesize}px; height:#{imagesize}px; position:absolute; top:#{y*ysize+20}px; left:#{30+x*xsize}px;'>"
+    puts "    <span style='position:absolute; top:#{y*ysize+50}px; left:#{x*xsize+ysize+6}px; font-size:10px;'>#{i}</span>"
   }
 }
 
